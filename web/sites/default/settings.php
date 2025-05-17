656b735a7ea6acdce510cb05e5da8b8437d42097
gnassiar <gia@PAPACITO>
2025-05-17 09:33:01 -0500

    robots y trusted

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -15,45 +15,11 @@
 
  $settings['container_yamls'][] = $app_root . '/' . $site_path . '/services.yml';
 
-/**
- * Trusted host configuration.
- *
- * Drupal core can use the Symfony trusted host mechanism to prevent HTTP Host
- * header spoofing.
- *
- * To enable the trusted host mechanism, you enable your allowable hosts
- * in $settings['trusted_host_patterns']. This should be an array of regular
- * expression patterns, without delimiters, representing the hosts you would
- * like to allow.
- *
- * For example:
- * @code
- * $settings['trusted_host_patterns'] = [
- *   '^www\.example\.com$',
- * ];
- * @endcode
- * will allow the site to only run from www.example.com.
- *
- * If you are running multisite, or if you are running your site from
- * different domain names (eg, you don't redirect http://www.example.com to
- * http://example.com), you should specify all of the host patterns that are
- * allowed by your site.
- *
- * For example:
- * @code
- * $settings['trusted_host_patterns'] = [
- *   '^example\.com$',
- *   '^.+\.example\.com$',
- *   '^example\.org$',
- *   '^.+\.example\.org$',
- * ];
- * @endcode
- * will allow the site to run off of all variants of example.com and
- * example.org, with all subdomains included.
- *
- * @see https://www.drupal.org/docs/installing-drupal/trusted-host-settings
- */
-# $settings['trusted_host_patterns'] = [];
+  $settings['trusted_host_patterns'] = [
+      '^45\\.61\\.60\\.195$',  // IP del servidor
+      '^munayco\\.com\\.pe$',  // Dominio principal
+      '^www\\.munayco\\.com\\.pe$',  // Subdominio www
+  ];
 
   $settings['file_scan_ignore_directories'] = [
     'node_modules',
